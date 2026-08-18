cb77b7bd54cacede4028d49484468db9cc15e165
Breno Macedo B. de Souza <brenomdev@protonmail.com>
2026-08-18 18:45:52 -0300

    work complete

---
+++ main.py
@@ -0,0 +1,16 @@
+import txt_analyser
+
+def main():
+    """
+    This code utilizes the class txt_analyser to read a .txt file and to
+    Display every word present in the file, in order of occurrence. From
+    The most used word to the least used word.
+    """
+    txt = txt_analyser.TxtAnalyser("theTXT.txt")
+    my_dict = txt.countWords()
+    print(my_dict)
+    txt.txtClose()
+
+
+if __name__ == "__main__":
+    main()
---
+++ txt_analyser.py
@@ -0,0 +1,56 @@
+class TxtAnalyser:
+    """
+    Class TxtAnalyser is used to read .txt files and count how many
+    times each word occurs in the file. It also organizes them
+    """
+    sorted_words = dict()
+    
+    def __init__(self, file_path):
+        """
+        Args:
+            file_path(str): full path to the .txt file including its
+            name. Example: "C:\Users\Username\Downloads\my_txt.txt"
+        """
+        self.file_path = file_path
+        self.file = open(file_path, "r+")
+
+    def getFilePath(self):
+        """
+        Returns: 
+            file_path(str): path of the .txt file.
+        """
+        return self.file_path
+    
+    def countWords(self):
+        """
+        Reads the .txt file, saves each word in a dictionary,
+        and how many times each occurred in the .txt file. And organizes
+        the words in decreasing order of amount of occurrence
+        Returns:
+            sorted_words(dictionary): dictionary in which keys are the
+            words present in the .txt file, and the values are each word
+            occurrence.
+        """
+        for line in self.file:
+           
+            for word in line.split():
+                
+                if word in self.sorted_words.keys():
+                    self.sorted_words[word] += 1
+                else:
+                    self.sorted_words[word] = 1
+
+        self.sorted_words = dict(
+            sorted(
+                self.sorted_words.items(), key=lambda item: item[1], 
+                reverse = True
+            )
+        )         
+        
+        return self.sorted_words
+
+    def txtClose(self):
+        """
+        closes the opened .txt
+        """
+        self.file.close()
